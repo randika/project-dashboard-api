024630ea4a21f48b6b1b6c067de27962946f837a
Silva <sasilva@wiley.com>
2020-05-06 14:04:51 +0530

    added dependecies for local logs, change the data type of createdAt to Date, added logs

--- project-dashboard-api/src/main/java/com/serverless/projects/GetListHandler.java
+++ project-dashboard-api/src/main/java/com/serverless/projects/GetListHandler.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.ApiGatewayResponse;
@@ -11,16 +13,22 @@ import com.serverless.Response;
 import com.serverless.model.Project;
 
 public class GetListHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse>{
-    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+	private Logger logger = Logger.getLogger(this.getClass());
+
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
     	try {
             List<Project> projects = new Project().list();
+         
             return ApiGatewayResponse.builder()
         				.setStatusCode(200)
         				.setObjectBody(projects)
         				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
         				.build();
         } catch (Exception ex) {
-      			Response responseBody = new Response("Error in fetching projects: ", input);
+        	
+      			logger.error("E002: Error in fetching the projects list: " + ex);
+
+      			Response responseBody = new Response("Error in fetching projects list: ", input);
       			return ApiGatewayResponse.builder()
       					.setStatusCode(500)
       					.setObjectBody(responseBody)
