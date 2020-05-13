ca4723f052f84e136b8df94a3c338258335c6dac
Randika Rathugamage <randika@randika.com>
2020-05-13 15:42:11 +0530

    added skelton

---
+++ project-dashboard-api/src/main/java/com/serverless/metrics/GetListHandler.java
@@ -0,0 +1,40 @@
+package com.serverless.metrics;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.serverless.ApiGatewayResponse;
+import com.serverless.Response;
+import com.serverless.model.Project;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class GetListHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        try {
+            List<Project> projects = new Project().list();
+
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(projects)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        } catch (Exception ex) {
+
+            logger.error("E002: Error in fetching the metrics list: " + ex);
+
+            Response responseBody = new Response("Error in fetching metrics list: ", input);
+
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
+}
