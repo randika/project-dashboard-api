86c242bce30b1c7809caf2c46fbef30a72f22dfc
Randika Rathugamage <randika@randika.com>
2020-05-19 21:32:48 +0530

    Update SonarMetricsHandler.java

--- src/main/java/com/serverless/webhooks/SonarMetricsHandler.java
+++ src/main/java/com/serverless/webhooks/SonarMetricsHandler.java
@@ -2,24 +2,42 @@ package com.serverless.webhooks;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.serverless.ApiGatewayResponse;
 import com.serverless.Response;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 import java.util.Collections;
 import java.util.Map;
 
 public class SonarMetricsHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-    //private static final Logger LOG = LogManager.getLogger(Handler.class);
+    private final Logger logger = Logger.getLogger(this.getClass());
 
     @Override
     public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-        //LOG.info("received: {}", input);
-        Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
-        return ApiGatewayResponse.builder()
-                .setStatusCode(200)
-                .setObjectBody(responseBody)
-                .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-                .build();
+        try {
+            JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
+            logger.error("saving metric: " + body);
+            
+            Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+                    .build();
+        } catch (Exception ex) {
+            logger.error("Error in saving metric: " + ex);
+
+            // send the error response back
+            Response responseBody = new Response("Error in saving sonar metric: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
     }
 }
