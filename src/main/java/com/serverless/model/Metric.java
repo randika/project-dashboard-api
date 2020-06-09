6ed8587184dc6c30905ef6d0437e4ec86343aa6f
Randika Rathugamage <randika@randika.com>
2020-06-09 12:26:29 +0530

    Create Metric.java

---
+++ src/main/java/com/serverless/model/Metric.java
@@ -0,0 +1,60 @@
+package com.serverless.model;
+
+import java.util.Date;
+
+public class Metric {
+    private String metricId;
+    private String metricType;
+    private String projectName;
+    private String teamId;
+    private String appId;
+    private Date createdAt;
+
+    public String getMetricId() {
+        return metricId;
+    }
+
+    public void setMetricId(String metricId) {
+        this.metricId = metricId;
+    }
+
+    public String getMetricType() {
+        return metricType;
+    }
+
+    public void setMetricType(String metricType) {
+        this.metricType = metricType;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
